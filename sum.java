a28c5e030f08227bba131ffdba34afaa838fb043
Apurbaa Halder <cse21029@cemk.ac.in>
2025-05-04 22:04:14 +0530

    main.java and sum.java file added

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int s=x+y;
+        System.out.println(s);
+        sc.close();
+    }
+}
